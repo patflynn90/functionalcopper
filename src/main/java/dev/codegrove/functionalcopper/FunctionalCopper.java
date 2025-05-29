8c2e9edd39894aed8b5ece67b4dc5ae29b62249f
Patrick Flynn <patflynn90@icloud.com>
2025-05-29 12:41:47 -0400

    initial commit

---
+++ src/main/java/dev/codegrove/functionalcopper/FunctionalCopper.java
@@ -0,0 +1,15 @@
+package dev.codegrove.functionalcopper;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FunctionalCopper implements ModInitializer {
+	public static final String MOD_ID = "functionalcopper";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/dev/codegrove/functionalcopper/FunctionalCopperClient.java
@@ -0,0 +1,10 @@
+package dev.codegrove.functionalcopper;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FunctionalCopperClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
